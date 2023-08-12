bc84aba640abaaf7fae423e103df38b76dc00994
Alexey Kozyrev <guper095@gmail.com>
2023-08-12 17:11:40 +0300

    Feat: Сделать секцию регистрация

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './NotFound.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <section>
-<h2>NotFound</h2>
+    <section className='not-found'>
+      <h1 className='not-found__title'>404</h1>
+      <p className='not-found__subtitle'>Страница не найдена</p>
+      <button 
+      className='not-found__button'
+      onClick={() => navigate(-1)}
+      type='button'>
+        Назад
+      </button>
     </section>
   );
 };
