a02ed3d31d82bdc5be593f25bee22a67d18ec02c
Alexey Kozyrev <guper095@gmail.com>
2023-08-12 17:23:14 +0300

    Feat: Сделать секцию Логин

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Login.css'
+import Form from '../Form/Form';
 
 const Login = () => {
   return (
-    <section>
-<h2>Login</h2>
+    <section className='login'>
+      <Form
+        title='Рады видеть!'
+        buttonName='Войти'
+        spanText='Еще не зарегистрированы?'
+        spanPatch='/signup'
+        spanLink='Регистрация'
+      >
+
+        <label className='form_label' htmlFor='email'>E-mail</label>
+        <input className='form__input' id='email' type='text' placeholder='Введите почту' />
+        <span className='form__span'> Что-то пошло не так </span>
+
+        <label className='form_label' htmlFor='password'> Пароль </label>
+        <input className='form__input' id='name' type='password' placeholder='Введите пароль' />
+        <span className='form__span'> Что-то пошло не так </span>
+      </Form>
     </section>
   );
 };
