69fd930aa68eff81d4005f16ca026c78d4c73530
Alexey Kozyrev <guper095@gmail.com>
2023-08-19 14:20:04 +0300

    Feat: LP (Descktop) PP

--- src/components/Landing/Portfolio/Portfolio.jsx
+++ src/components/Landing/Portfolio/Portfolio.jsx
@@ -8,33 +8,36 @@ const Portfolio = () => {
       <h2 className='portfolio__title'>Портфолио</h2>
       <ul className='portfolio__list'>
         <li className='portfolio__item'>
-          <p className='portfolio__name'>Статический сайт</p>
+          {/* <p className='portfolio__name'>Статический сайт</p> */}
             <Link 
             className='portfolio__link'
             to={'https://kozyrevalexey.github.io/Learn-to-learn/'}
             target='_blank'
             rel='noreferrer'>
-              <span className='portfolio__icon'></span>
+             <p className='portfolio__name'>Статичный сайт</p>
+              <span className='portfolio__icon'>↗</span>
             </Link>
         </li>
         <li className='portfolio__item'>
-          <p className='portfolio__name'>Адаптивный сайт</p>
+          {/* <p className='portfolio__name'>Адаптивный сайт</p> */}
             <Link 
             className='portfolio__link'
             to={'https://kozyrevalexey.github.io/russian-travel/index.html'}
             target='_blank'
             rel='noreferrer'>
-              <span className='portfolio__icon'></span>
+            <p className='portfolio__name'>Адаптивный сайт</p>
+              <span className='portfolio__icon'>↗</span>
             </Link>
         </li>
         <li className='portfolio__item'>
-          <p className='portfolio__name'>Одностраничное приложение</p>
+          {/* <p className='portfolio__name'>Одностраничное приложение</p> */}
             <Link 
             className='portfolio__link'
             to={'https://kozyrevalexey.github.io/mesto/'}
             target='_blank'
             rel='noreferrer'>
-              <span className='portfolio__icon'></span>
+                    <p className='portfolio__name'>Одностраничное приложение</p>
+              <span className='portfolio__icon'>↗</span>
             </Link>
         </li>
       </ul>
--- src/components/Navigation/NavigationLanding/NavigationLanding.jsx
+++ src/components/Navigation/NavigationLanding/NavigationLanding.jsx
@@ -9,16 +9,14 @@ const NavigationLanding = () => {
         <Link
         className='nav__link nav__landing-reg'
         to='/signup'
-        tabIndex={1}
         >
         Регистрация
         </Link>
         <Link
         className='nav__link nav__landing-enter'
         to='/signup'
-        tabIndex={1}
         >
-        Регистрация
+        Войти
         </Link>
       </div>
     </div>
